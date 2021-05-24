492c4d0dd6cb69559ef04d1a306bf036ef446831
rainbowdango <minhkhoi1026@gmail.com>
2021-05-24 17:18:06 +0700

    Adding respond method of server and request method of client (haven't tested)

--- socket/library_client.py
+++ socket/library_client.py
@@ -1,4 +1,5 @@
 import socket
+import json
 import utils
 
 HOST = "127.0.0.1"  # The default server's hostname or IP address
@@ -14,11 +15,11 @@ class library_client:
 #-------------------PUBLIC AREA-------------------
     def connect(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
         while True:
             try:
                 server_addr = (self.host, self.port)
                 self.server = socket.create_connection(server_addr, timeout = 0.5)
+                self.server = utils.socket_adapter(server) # wrap socket in different interface
                 print("Connected successfullt at", utils.host_to_str(*server_addr))
                 break
             except socket.error:
@@ -30,8 +31,37 @@ class library_client:
     def close_connect(self):
         try:
             self.server.send("QUIT".encode("utf-8"))
+            self.server.close()
+            return True
         except:
-            pass
+            return False
+    
+    def log_in(self, username, password):
+        request = ' '.join(["LOGIN", username, password])
+        self.server.send(request.encode("utf-8"))
+        msg = self.server.recv().decode("utf-8")
+        return eval(msg)
+    
+    def sign_up(self, username, password):
+        request = ' '.join(["SIGNUP", username, password])
+        self.server.send(request.encode("utf-8"))
+        msg = self.server.recv().decode("utf-8")
+        return eval(msg)
+    
+    def get_list_book(self, cmd, detail):
+        if (cmd not in ["F_ID", "F_NAME", "F_TYPE", "F_AUTHOR"]):
+            return []
+        request = ' '.join([cmd], detail)
+        self.server.send(request.encode('utf-8'))
+        json_books = self.server.recv().decode('utf-8')
+        books = json.loads(json_books)
+        return books
+    
+    def get_book_content(self, id):
+        request = ' '.join(["GETBOOK"], id)
+        self.server.send(request.encode('utf-8'))
+        content = self.server.recv()
+        return content
 
 client = library_client()
 client.connect()
